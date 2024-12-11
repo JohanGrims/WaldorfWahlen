b25cdab7c6da6395433782326086a714c7786d45
JohanGrims <johan.grimsehl@gmail.com>
2024-12-11 13:57:53 +0100

    Remove unnecessary JSDoc comment from CheckItem component

--- src/CheckItem.tsx
+++ src/CheckItem.tsx
@@ -5,7 +5,7 @@ import React from "react";
     * CheckItem component
     * @param {string} label - The label of the checkbox
     * @param {boolean} checked - The state of the checkbox
-    */
+*/
 export default function CheckItem({
   label,
   checked,
