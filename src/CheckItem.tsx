448b46df3a3fe5c124669b2b7d8d2972a9808d1d
JohanGrims <johan.grimsehl@gmail.com>
2024-12-11 13:57:18 +0100

    Enhance CheckItem component with TypeScript types for props

--- src/CheckItem.tsx
+++ src/CheckItem.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 
-export default function CheckItem({ label, checked }) {
+
+/*
+    * CheckItem component
+    * @param {string} label - The label of the checkbox
+    * @param {boolean} checked - The state of the checkbox
+    */
+*/
+export default function CheckItem({
+  label,
+  checked,
+}: {
+  label: string;
+  checked: boolean;
+}) {
   if (checked) {
     return (
       <mdui-checkbox disabled checked checked-icon="done">
