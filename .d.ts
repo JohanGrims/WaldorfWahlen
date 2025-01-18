e1f04499c315661e20638e8e1a31952e25f77c81
johangrims <johan.grimsehl@gmail.com>
2025-01-18 10:25:33 +0100

    Add typescript declaration file

---
+++ .d.ts
@@ -0,0 +1,21 @@
+/// <reference types="mdui/jsx.en.d.ts" />
+
+declare namespace JSX {
+    interface IntrinsicElements {
+      "mdui-radio-group": any;
+      "mdui-radio": any;
+      "mdui-text-field": any;
+      "mdui-button": any;
+      "mdui-button-icon": any;
+      "mdui-tooltip": any;
+      "mdui-tab": any;
+      "mdui-badge": any;
+      "mdui-tabs": any;
+      "mdui-tab-panel": any;
+      "mdui-segmented-button-group": any;
+      "mdui-segmented-button": any;
+      "mdui-list": any;
+      "mdui-list-item": any;
+    }
+  }
+  
