933eaeea38a1bc7102ea87054e985417ee315f8b
JohanGrims <johan.grimsehl@gmail.com>
2025-02-09 00:29:33 +0100

    Update package.json dependencies and reformat JSX in Result.jsx (#49)
    * Update dependencies
    
    * Update dependencies and refactor Gateway component to use 'replace' for navigation

--- src/Result.jsx
+++ src/Result.jsx
@@ -34,11 +34,10 @@ export default function Result() {
         <div className="button-container">
           <mdui-button onClick={() => navigate("/")}>Startseite</mdui-button>
           {localStorage.getItem(id)?.choiceId && (
-          <mdui-button disabled variant="text">
-            {JSON.parse(localStorage.getItem(id))?.choiceId}
-          </mdui-button>
-          )
-          }
+            <mdui-button disabled variant="text">
+              {JSON.parse(localStorage.getItem(id))?.choiceId}
+            </mdui-button>
+          )}
         </div>
       </mdui-dialog>
     );
