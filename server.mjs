a08c125ad5975eeeb0f17355e7759fe687159461
johangrims <johan.grimsehl@gmail.com>
2025-08-22 17:29:01 +0200

    Add backend configuration and server setup with Express

---
+++ server.mjs
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+// PORT env var is set by Cloud Run
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
